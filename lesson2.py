6e1b9a6facdc07032e618cbd1380e157de848b76
Anna <112608030+judashair@users.noreply.github.com>
2022-09-02 14:21:00 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,29 @@
+"""
+author: Anna Samsonenko
+date: 02.09.2022
+
+"""
+# lesson2_task1
+my_name = "Anna"
+day = "friday"
+print(f"Good day, {my_name}! {day.title()} is a perfect day to learn some python.")
+
+# lesson2_task2
+name = "Anna"
+vorname = "Samsonenko"
+greeting = "Good evening, " + name + " " + vorname + "!"
+print(greeting)
+
+# lesson2_task3
+a = 15
+b = 23
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+print(a ** b)
+print(a // b)
+print(a % b)
+
+
+
