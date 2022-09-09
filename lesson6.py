1b458c0a3067f67709410ce844770db109636ea9
Anna <112608030+judashair@users.noreply.github.com>
2022-09-09 19:42:23 +0300

    Update lesson6.py

--- lesson6.py
+++ lesson6.py
@@ -11,13 +11,16 @@ print(max(list_of_numbers))
 # task2
 from random import randint
 
-list_1, list_2, list_3 = [randint(0, 100) for _ in range(10)], [randint(0, 100) for _ in range(10)], []
-x = 0
-while x < 10:
-    if list_1[x] in list_2:
-        list_3.append(list_1[x])
-    x += 1
-print(list_3 if list_3 else "No duplicates.")
+list1, list2, list3 = [randint(0, 100) for i in range(10)], [randint(0, 100) for i in range(10)], []
+i = 0
+print(list1)
+print(list2)
+while i < 10:
+    if list1[i] in list2:
+        list3.append(list1[i])
+    i += 1
+
+print(list3 if list3 else "Повторень немає.")
 
 # task3
 list_1, list_2 = list(range(1, 101)), []
