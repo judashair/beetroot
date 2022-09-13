cb0ff5c5a66d0dc668f5fadbcdae8704cb5075b1
Anna <112608030+judashair@users.noreply.github.com>
2022-09-13 20:10:05 +0300

    Create lesson8.py

---
+++ lesson8.py
@@ -0,0 +1,31 @@
+# task 1
+def favorite_movie(name)
+    print(f"My favorite movie is named {name}")
+
+favorite_movie("Harry Potter")
+
+# task 2
+def make_country(capital, name):
+    country_dict = {'country_name': name, 'capital': capital}
+    return country_dict
+
+print(make_country("Warsaw", "Poland"))
+
+# task 3
+def make_operation(symbol, *args):
+    x = 0
+    if symbol == "+":
+        for numbers in args:
+            x += numbers
+    elif symbol == "-":
+        for numbers in args:
+            x -= numbers
+    elif symbol == "*":
+        x = 1
+        for numbers in args:
+            x *= numbers
+    return print(x)
+
+make_operation("+", 7, 12, 2)
+make_operation("-", 5, 45, -10, -20,)
+make_operation("*", 6, 9)
