c27951c17a0ed24e2967e125fcd36850a47f50cf
Anna <112608030+judashair@users.noreply.github.com>
2022-09-19 20:06:18 +0300

    Add files via upload

---
+++ lesson9/3/task_3.py
@@ -0,0 +1,4 @@
+from mymod import test
+
+some_text = "PYTHONPATH is an environment variable \n which you can set to add additional directories \n where python will look for modules and packages."
+test(some_text)
