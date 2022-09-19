f9518fcdbfc3b72bf749aa6ebef5a3f85e438d85
Anna <112608030+judashair@users.noreply.github.com>
2022-09-19 18:20:59 +0300

    Update task_2.py

--- lesson9/task_2.py
+++ lesson9/task_2.py
@@ -2,3 +2,11 @@ import sys
 
 for i in sys.path:
     print(i)
+
+
+# C:\Users\Аня\PycharmProjects\pythonProject2
+# C:\Users\Аня\PycharmProjects\pythonProject2
+# C:\Program Files\JetBrains\PyCharm 2022.2.1\plugins\python\helpers\pycharm_display
+# C:\Users\Аня\PycharmProjects\pythonProject2\venv\Scripts\python38.zip
+# D:\python\DLLs
+# D:\python\lib
