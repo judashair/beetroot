9d8495c5db0e910c92afee1a5ffe392960dc3dc7
Anna <112608030+judashair@users.noreply.github.com>
2022-09-20 10:24:47 +0300

    Update mymod.py

--- lesson9/3/mymod.py
+++ lesson9/3/mymod.py
@@ -1,18 +1,18 @@
-import os
-
 def count_lines(name):
-    lines = 0
-    for char in name:
-        if "\n" in char:
-        	lines += 1
-    return lines
+	lines: int = 0
+	for char in name:
+		if "\n" in char:
+			lines += 1
+	return lines
+
 
 def count_chars(name: str):
 	name = name.replace(' ', "")
 	ch = 0
-	for char in name:
+	for _ in name:
 		ch += 1
 	return ch
+
 
 def test(name):
 	print(f"There are {count_lines(name)} lines and {count_chars(name)} chars.")
