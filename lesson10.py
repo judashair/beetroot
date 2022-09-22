a716823a0d9d3e6a42b42efc8e97f739ddcbc76f
Anna <112608030+judashair@users.noreply.github.com>
2022-09-22 14:21:47 +0300

    Add files via upload

---
+++ lesson10.py
@@ -0,0 +1,34 @@
+def kwadrat_div():
+    a = input('Enter number a: ')
+    b = input('Enter number b: ')
+
+    try:
+        int(a)**2/int(b)
+    except ValueError:
+        print('Your input is not digit!')
+    except ZeroDivisionError:
+        print("You try to divide by zero! Enter the other value of 'b'!")
+    else:
+        return print('The answer is: ' + str(int(a)**2/int(b)))
+
+
+kwadrat_div()
+
+def oops_index_error():
+    raise IndexError
+
+
+def oops_key_error():
+    raise KeyError
+
+
+def another_function():
+    names = {'Lviv': 1, "Warsaw": 2}
+    try:
+        print(names['Berlin'])
+        print("No IndexErrors")
+    except:
+        oops_index_error()
+
+
+another_function()
