7f9b8162276efe6f800d40bea8171d6e2d18eb4d
Anna <112608030+judashair@users.noreply.github.com>
2022-10-15 13:38:48 +0300

    Update task3.py

--- lesson17/task3.py
+++ lesson17/task3.py
@@ -1,26 +1,53 @@
 class Fraction:
 
     def __init__(self, numerator: int, denominator: int):
-        self.den = denominator
-        self.num = numerator
+        self.denominator = denominator
+        self.numerator = numerator
 
-    def __add__(self, other):
-        return Fraction(self.num * other.den + self.den * other.num, self.den * other.den)
-
-    def __sub__(self, other):
-        return Fraction(self.num * other.den - other.num * self.den, self.den * other.den)
-        pass
-
-    def __truediv__(self, other):
-        return self.__mul__(Fraction(other.den, other.num))
-
-    def __mul__(self, other):
-        return Fraction(self.num * other.num, self.den * other.den)
+        if self.numerator == 0 or self.denominator == 0:
+            raise ValueError("Numerator and denominator have to be not a zero")
 
     def __str__(self):
-        return f"{self.num}\n——\n{self.den}\n"
+        return f'{self.numerator}/{self.denominator}'
 
-# if __name__ == '__main__':
+    def reduction(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+        a = 0
+        while a < 5:
+            for i in range(2, 10):
+                if self.numerator % i == 0 and self.denominator % i == 0:
+                    self.numerator = int(self.numerator / i)
+                    self.denominator = int(self.denominator / i)
+                continue
+            a += 1
+        return f'{self.numerator}/{self.denominator}'
+
+    def __add__(self, other):
+        self.numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        self.denominator = self.denominator * other.denominator
+        self.reduction(self.numerator, self.denominator)
+        return f'{self.numerator}/{self.denominator}'
+
+    def __sub__(self, other):
+        self.numerator = self.numerator * other.denominator - other.numerator * self.denominator
+        self.denominator = self.denominator * other.denominator
+        self.reduction(self.numerator, self.denominator)
+        return f'{self.numerator}/{self.denominator}'
+
+    def __mul__(self, other):
+        self.numerator = self.numerator * other.numerator
+        self.denominator = self.denominator * other.denominator
+        self.reduction(self.numerator, self.denominator)
+        return f'{self.numerator}/{self.denominator}'
+
+    def __truediv__(self, other):
+        self.numerator = self.numerator * other.denominator
+        self.denominator = self.denominator * other.numerator
+        self.reduction(self.numerator, self.denominator)
+        return f'{self.numerator}/{self.denominator}'
+
+
 f1 = Fraction(1, 2)
 f2 = Fraction(1, 4)
 print(f1 + f2)
